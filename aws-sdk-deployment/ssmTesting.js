1108c442c14720b54e8c118058f1e6968ce23ae2
cloud303-mpena <mpena@cloud303.io>
2025-05-23 09:20:51 -0600

    deleted some files

--- aws-sdk-deployment/ssmTesting.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var resGen = require("./resource-generator");
-// Current error that needs to be solved: https://docs.aws.amazon.com/sdk-for-ruby/v3/api//Aws/SSM/Types/InvalidInstanceId.html
-// Current fix, enable SSM in web console
-resGen.configureYugabyteNodes("i-08cedcf2853fa161c", "ubuntu", "us-east-1", ["us-east-1a", "us-east-1b", "us-east-1c"], ["10.0.0.151", "10.0.1.66", "10.0.2.130"], 3);
